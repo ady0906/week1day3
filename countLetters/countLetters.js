643837d5b7111ba6ac1f1b5807b1f5052cdcaa99
Adrien Peynichou <adrien.peynichou@gmail.com>
2017-01-11 16:20:00 +0000

    Counting letters with objects

---
+++ countLetters/countLetters.js
@@ -0,0 +1,15 @@
+function countLetters(str) {
+  var workStr = str.split(" ").join("").split("");
+  var finalObj = new Object();
+  for (var i = 0; i < workStr.length; i++) {
+    if (!finalObj.hasOwnProperty(workStr[i])) {
+      finalObj[workStr[i]] = 1;
+    }
+    else {
+      finalObj[workStr[i]] += 1;
+    }
+  }
+  return finalObj;
+}
+
+console.log(countLetters("lighthouse in the house"));
